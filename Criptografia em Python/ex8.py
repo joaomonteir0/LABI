6234eb820ebb5586c9aca2fcd8dbaa863ce45b65
João Monteiro <73401053+joaomonteir0@users.noreply.github.com>
2022-03-09 12:49:28 +0000

    a

--- Criptografia em Python/ex8.py
+++ Criptografia em Python/ex8.py
@@ -4,17 +4,15 @@ import sys
 import os
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
+import random
+import string
+
 
 if len(sys.argv) < 3:
-    print("Invalid arguments!\n must be:\n>>> python3 ex8.py ficheiro.txt chave > criptograma")
+    #print("Invalid arguments!\n must be:\n>>> python3 ex8.py ficheiro.txt chave > criptograma")
     exit(1)
-
-f = open(sys.argv[1], "r")
-text = f.read()
-
 chave = sys.argv[2]
-print(text)
-print(chave)
+
 if len(chave) < 16:
     novaChave = SHA256.new()
     novaChave.update(chave.encode("utf-8"))
@@ -39,4 +37,14 @@ elif len(chave) > 32:
     novaChave = novaChave.hexdigest()
     novaChave= novaChave[0:32]
     chave = novaChave
-print(chave)
+
+f = open(sys.argv[1], "rb")
+text = f.read()
+f.close()
+
+cryptogram = ""
+cipher = AES.new(chave)
+
+for i in range(0,int(len(text)/16)):
+    cryptogram = cipher.encrypt(str(text[i*16:(i+1)*15]))
+    print(cryptogram)
